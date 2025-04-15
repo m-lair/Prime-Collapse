09f89c2e488a4ece830781923b80935da224c854
m-lair <mlair@heidelberg.edu>
2025-04-15 17:46:23 -0500

    Initial Commit

---
+++ Prime Collapse/Prime_CollapseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prime_CollapseApp.swift
+//  Prime Collapse
+//
+//  Created by Marcus Lair on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Prime_CollapseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
